a21eedffef21ac455dc86f14ee19ef59d88ae9eb
Radionic <khpluslt@gmail.com>
2022-08-05 00:22:44 +0800

    Support saving best loss

--- CP/model.py
+++ CP/model.py
@@ -30,7 +30,7 @@ class CP(object):
         midi_obj = utils.convert_string_quartets(midi_obj)
         if not utils.is_string_quartets(midi_obj):
             return None
-    
+        
         numerator = midi_obj.time_signature_changes[0].numerator
         if self.task == "custom" or self.task == "skyline":
             # Add 'Program' to each raw token
@@ -65,13 +65,10 @@ class CP(object):
 
         return events
 
-    def padding(self, data, ans):
+    def padding(self, data):
         pad_len = self.max_len - len(data)
         for _ in range(pad_len):
-            if not ans:
-                data.append(self.pad_word)
-            else:
-                data.append(0)
+            data.append(self.pad_word)
         return data
 
     def _prepare_data(self, path):
@@ -95,7 +92,7 @@ class CP(object):
                 for i in range(0, len(words), self.max_len):
                     slice_words.append(words[i : i + self.max_len])
                 if len(slice_words[-1]) < self.max_len:
-                    slice_words[-1] = self.padding(slice_words[-1], ans=False)
+                    slice_words[-1] = self.padding(slice_words[-1])
             elif self.task == "skyline":
                 slice_words, slice_ys = self.skyline.generate(words)
 
