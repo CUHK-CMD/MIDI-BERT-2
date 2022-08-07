3ac915bf24a470583731a975ac754de7570aab55
tpmmthomas <71880685+tpmmthomas@users.noreply.github.com>
2022-08-07 19:55:10 +0800

    Update model.py

--- MidiBERT/model.py
+++ MidiBERT/model.py
@@ -104,7 +104,7 @@ class MidiBertSeq2Seq(nn.Module):
             )
         elif ckpt_s2s is not None:
             self.bert2bert = EncoderDecoderModel(config=config)
-            checkpoint = torch.load(f"./result/seq2seq/{ckpt}/model_best.ckpt")
+            checkpoint = torch.load(f"./result/seq2seq/{ckpt_s2s}/model_best.ckpt")
             for key in list(checkpoint["state_dict"].keys()):
                 # rename the states in checkpoint
                 checkpoint["state_dict"][key.replace("module.", "")] = checkpoint[
